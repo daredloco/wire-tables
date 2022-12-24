dc367415fe5b711dcfc339470ac3eaa6c1f43249
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-24 17:26:19 -0300

    Update DataTable.php

--- src/DataTables/DataTable.php
+++ src/DataTables/DataTable.php
@@ -60,7 +60,7 @@ class DataTable{
             throw new Exception('Model "'.$model.'" is not a valid Illuminate\\Database\\Eloquent\\Model!');
         }
         $table = new DataTable();
-        $columns = Schema::getColumnListing($model->getTable());
+        $columns = Schema::getColumnListing((new $model())->getTable());
         $models = $model::all();
         
         foreach($columns as $column){
