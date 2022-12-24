cc4b3f18a6befcd9e7e3addd46e3aaa40f459e1f
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-24 20:04:22 -0300

    Update wire-table.blade.php

--- resources/views/livewire/wire-table.blade.php
+++ resources/views/livewire/wire-table.blade.php
@@ -12,31 +12,10 @@
                 @foreach($rows as $row)
                     <tr class="">
                         @foreach($row as $colIdx => $colContent)
-                        {{ dd($column, $colContent, $row) }}
                             @if($firstBold && $colIdx == 0)
-                                @if($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Boolean)
-                                    <th scope="row">{{ $colContent == true ? __('True') : __('False') }}</th>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Numeric)
-                                    <th scope="row">{{ round($colContent,2) }}</th>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Date)
-                                    <th scope="row">{{ (new \Carbon\Carbon($colContent))->format('d/m/Y') }}</th>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Currency)
-                                    <th scope="row">R${{ number_format($colContent,2,',','.') }}</th>
-                                @else
-                                    <th scope="row">{{ $colContent }}</th>
-                                @endif
-                            @else
-                                @if($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Boolean)
-                                    <td scope="row">{{ $colContent == true ? __('True') : __('False') }}</td>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Numeric)
-                                    <td scope="row">{{ round($colContent,2) }}</td>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Date)
-                                    <td scope="row">{{ (new \Carbon\Carbon($colContent))->format('d/m/Y') }}</td>
-                                @elseif($column['cast'] == \Helvetiapps\WireTables\Enums\Casts::Currency)
-                                    <td scope="row">R${{ number_format($colContent,2,',','.') }}</td>
-                                @else
-                                    <td scope="row">{{ $colContent }}</td>
-                                @endif
+                                <th scope="row">{{ $colContent }}</th>
+                            @else      
+                                <td scope="row">{{ $colContent }}</td>
                             @endif
                         @endforeach
                     </tr>
